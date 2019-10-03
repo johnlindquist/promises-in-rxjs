00777a91e6f564bd6d26a39815e1e5ba68d3490b
John Lindquist <johnlindquist@gmail.com>
2019-10-03 12:09:29 -0600

    Thu Oct  3 12:09:29 MDT 2019

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+import { fromEvent, pipe } from "rxjs"
+import { switchMap } from "rxjs/operators"
+
+const getLuke = pipe(
+  switchMap(event =>
+    fetch(`https://johnlindquist-swapi-json-server-2.glitch.me/people/1`)
+  ),
+  switchMap(response => response.json())
+)
+
+fromEvent(document, "click")
+  .pipe(getLuke)
+  .subscribe(console.log)
